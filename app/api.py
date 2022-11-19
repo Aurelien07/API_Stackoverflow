bfe3a3314c818a121efb025ae1fd8c05a1048ae2
Aurélien <87898572+Aurelien07@users.noreply.github.com>
2022-11-20 00:22:30 +0200

    API for stackoverflow

---
+++ app/api.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# uvicorn main:app -> pour lancer l'API
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/", tags=["Home"])
+def get_root() -> dict:
+    returrn {
+        "message": "API de detection des TAGS StackOverFlow :"
+    }
